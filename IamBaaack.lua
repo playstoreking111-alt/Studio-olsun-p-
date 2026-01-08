546c2c6f22332139828d7d3691971cc1e7f2a8d5
playstoreking111-alt <playstoreking111@gmail.com>
2026-01-08 16:54:10 +0300

    Add files via upload

--- IamBaaack.lua
+++ IamBaaack.lua
@@ -641,7 +641,7 @@ char:FindFirstChild("TalkingBillBoard"):destroy()
 	refused:Stop()
 		wait(2)
 
-chatfunc("But It Refused")
+chatfunc("Ama Bu Reddedildi =)")
 		wait(1.5)
 		candie = true
 		idle = true
@@ -830,12 +830,12 @@ soul6sweld.Part0 = torso
 soul6sweld.Part1 = soul6s
 soul6sweld.C0 = CFrame.new(6,-1,0)
 
-chatfunc("These Are The Determinations")
+chatfunc("Bunlar Azim")
 wait(2)
-chatfunc("I Never Got To Extract")
+chatfunc("Öldürmekten Başka bir şey yapmadım.")
 wait(2)
 
-chatfunc("In The Past Genocides")
+chatfunc("Geçmiş Soykırımlarda")
 Weldb.C1 = CFrame.new(0, -2, 0) * CFrame.fromEulerAnglesXYZ(-3, 0, 0)
 for i = 1,100 do
 	wait()
@@ -846,7 +846,7 @@ for i = 1,100 do
         rleg.Weld.C0 = Lerp(rleg.Weld.C0, CFrame.new(0.5, -1, 0) * CFrame.Angles(math.rad(0), 0, math.rad(4)), 0.01)
  
 end
-chatfunc("Lets See How Long You Survive")
+chatfunc("Hadi Bakalım Ne Kadar Yaşayacaksın =)")
 
 for i = 1,10 do
 	wait(0.04)
@@ -1102,7 +1102,7 @@ soul4sd:Destroy()
 soul5sd:Destroy()
 soul6sd:Destroy()
 idle = true
-chatfunc("Come =)")
+chatfunc("GEL =)")
 hed.face.Texture = "http://www.roblox.com/asset/?id=946610608"
 lovecounter = true
 walking = true
@@ -1190,7 +1190,7 @@ mouse.KeyDown:connect(function(key)
 		colorred = colorred + 0.006
 		handle.Color = Color3.new(colorred,0,0)
 		end
-		chatfunc("Welcome To My Special Hell")
+		chatfunc("Benim cehennemime hoş geldiniz")
 		for i = 1,120 do
 			wait()
 			kill.Transparency = kill.Transparency - 0.005
@@ -1746,7 +1746,7 @@ what.CanCollide = false
 function onTouched(hit)
 			if hit.Parent:FindFirstChild("Immune") == nil then
 				if hit.Parent:FindFirstChild("Torso") ~= nil  then
-					chatfunc('I Got You')
+					chatfunc('Yakaladım Seni.')
 				
 					t = hit.Parent:FindFirstChild("Torso")
 					h = hit.Parent:FindFirstChild("Head")
